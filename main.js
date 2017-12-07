ba26da3dbd3b34f99f83556e9aca17353feeca31
Ryan Leung <leung@Ryans-MacBook-Pro-2.local>
2017-12-07 03:04:28 -0500

    adding highlight/ test pdf

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const electron = require('electron')
 const app = electron.app
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
+const {ipcMain} = electron;
 
 const path = require('path')
 const url = require('url')
@@ -13,10 +14,12 @@ const url = require('url')
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
+let folderWindow
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({height:1080,width:1920,frame :false})
+  mainWindow = new BrowserWindow({ height: 768, width: 1024, frame: false })
+
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
@@ -24,6 +27,8 @@ function createWindow () {
     protocol: 'file:',
     slashes: true
   }))
+
+
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
@@ -35,7 +40,11 @@ function createWindow () {
     // when you should delete the corresponding element.
     mainWindow = null
   })
+
+
 }
+
+
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
@@ -46,9 +55,9 @@ app.on('ready', createWindow)
 app.on('window-all-closed', function () {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+
+  app.quit()
+
 })
 
 app.on('activate', function () {
